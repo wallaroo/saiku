53a6dfb0048402015c4fe855d36f8cc1509d5d33
pstoellberger <pstoellberger@gmail.com>
2012-10-25 19:46:10 +0200

    make mondrian 4 build

--- buildall.sh
+++ buildall.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+curl https://raw.github.com/pentaho/mondrian/lagunitas/demo/FoodMart.mondrian.xml > util/FoodMart.xml
+cp util/FoodMart.xml saiku-core/saiku-service/src/test/resources/org/saiku/olap/discover/FoodMart.xml
 cd saiku-core
-mvn clean install  
-#-DskipTests=true 
+mvn clean install -DskipTests=true 
 cd ..
 cd saiku-webapp
 mvn clean install
@@ -11,7 +12,7 @@ git submodule update
 cd saiku-ui
 git checkout master 
 git pull
-mvn clean package install:install-file -Dfile=target/saiku-ui-2.5-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=2.5-SNAPSHOT -Dpackaging=war
+mvn clean package install:install-file -Dfile=target/saiku-ui-2.5-SNAPSHOT.war  -DgroupId=org.saiku -DartifactId=saiku-ui -Dversion=MONDRIAN4-SNAPSHOT -Dpackaging=war
 cd ../saiku-server
 mvn clean package
 cd ../saiku-bi-platform-plugin-util
